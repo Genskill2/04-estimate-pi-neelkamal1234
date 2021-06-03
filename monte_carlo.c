baccaa19d35e72bb90b6f48ce0f9ba191495900d
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-03 13:35:14 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -43,16 +43,18 @@ float mc_pi(int n)
 {  int c =0;
    int s =0 ;
    int snew ;
-  float x,y ;  
-for( int i=0; i<=n :i++)
-{ x = frandom();
-  y = frandom();
- if( (pow(x,2)+pow(y,2)) <= 1)
- { c++;}
- else { s++;}
-}
-snew = s+c ;
-float pi;
- pi = (float)(4*(c/snew)) ;
- return pi;
+   float x;
+   float y ;
+ 
+   for( int i=0; i<=n ;i++)
+      { x = frandom();
+         y = frandom();
+          if( (pow(x,2)+pow(y,2)) <= 1)
+              { c++ ;}
+            else { s++ ;}
+       }
+   snew = s+c ;
+   float pi;
+   pi = (float)(4*(c/snew)) ;
+   return pi;
 }
